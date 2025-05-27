3ae9caf14fb4c024f54c1c99f94e12a6e3da0f3a
NguyenVoTruongGiang <21130042@st.hcmuaf.edu.vn>
2025-05-27 22:26:56 +0700

    update order

--- src/main/java/com/example/be_deliveryfood/dto/request/OrderItemRequest.java
+++ src/main/java/com/example/be_deliveryfood/dto/request/OrderItemRequest.java
@@ -1,7 +1,5 @@
-package com.example.be_deliveryfood.service;
+package com.example.be_deliveryfood.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
--- src/main/java/com/example/be_deliveryfood/dto/request/OrderRequest.java
+++ src/main/java/com/example/be_deliveryfood/dto/request/OrderRequest.java
@@ -1,6 +1,5 @@
 package com.example.be_deliveryfood.dto.request;
 
-import com.example.be_deliveryfood.service.OrderItemRequest;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
